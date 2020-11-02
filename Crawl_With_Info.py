e62a513998c637c0c403bbf0a547bc89a5fe82f5
Toiongo <45739624+Toiongo@users.noreply.github.com>
2020-11-02 12:51:48 +0300

    Update Crawl_With_Info.py

--- Crawl_With_Info.py
+++ Crawl_With_Info.py
@@ -10,41 +10,40 @@ logging.basicConfig(level=logging.INFO)
 
 
 def main():
-    async def magnet_to_torrent(magnet_uri, dst):
-        params = libtorrent.parse_magnet_uri(magnet_uri)
-
-        session = libtorrent.session({'dht_announce_interval': 3})
-        handle = session.add_torrent(params)
-        info = handle.status()
-        counter = time.time()
-        print("Trying magnet link: '" + magnet_uri + "'...")
-        while not handle.has_metadata():
-            info = handle.status()
-
-            if info.num_peers > 0:
-                counter = time.time()
-
-            temp = time.time()
-            print("\rRemaining time: '{}|60' /=/ Number of peers: {}".format(int(temp - counter), str(info.num_peers)), end="")
-            time.sleep(0.01)
-
-            # You can adjust the time program waits to get a torrent file, just change 60 to amount of seconds of your choice.
-            # (You might also want to modify the print msg above that says |60 seconds)
-            # And you might also want to remove the peer condition below, to speed things up.
-            if temp - counter >= 60 and info.num_peers == 0:
-                print("\nCouldn't find peers for '" + magnet_uri + "' after " + str(int(temp - counter)) + " seconds.")
-                return
-
-        torrent_info = handle.get_torrent_info()
-        torrent_file = libtorrent.create_torrent(torrent_info)
-        torrent_path = os.path.join(dst, ''.join(e for e in torrent_info.name() if e.isalnum() or e in "[] ") + ".torrent")
-        with open(torrent_path, "wb") as f:
-            f.write(libtorrent.bencode(torrent_file.generate()))
-        print("\nTorrent saved to %s" % torrent_path)
-        return
-
-
     class Crawler(Maga):
+	async def magnet_to_torrent(magnet_uri, dst):
+	    params = libtorrent.parse_magnet_uri(magnet_uri)
+
+	    session = libtorrent.session({'dht_announce_interval': 3})
+	    handle = session.add_torrent(params)
+	    info = handle.status()
+	    counter = time.time()
+	    print("Trying magnet link: '" + magnet_uri + "'...")
+	    while not handle.has_metadata():
+	        info = handle.status()
+
+	        if info.num_peers > 0:
+		     counter = time.time()
+
+	        temp = time.time()
+	        print("\rRemaining time: '{}|60' /=/ Number of peers: {}".format(int(temp - counter), str(info.num_peers)), end="")
+	        time.sleep(0.01)
+
+	        # You can adjust the time program waits to get a torrent file, just change 60 to amount of seconds of your choice.
+	        # (You might also want to modify the print msg above that says |60 seconds)
+	        # And you might also want to remove the peer condition below, to speed things up.
+	        if temp - counter >= 60 and info.num_peers == 0:
+		     print("\nCouldn't find peers for '" + magnet_uri + "' after " + str(int(temp - counter)) + " seconds.")
+		     return
+
+	     torrent_info = handle.get_torrent_info()
+	     torrent_file = libtorrent.create_torrent(torrent_info)
+	     torrent_path = os.path.join(dst, ''.join(e for e in torrent_info.name() if e.isalnum() or e in "[] ") + ".torrent")
+	     with open(torrent_path, "wb") as f:
+	        f.write(libtorrent.bencode(torrent_file.generate()))
+	    print("\nTorrent saved to %s" % torrent_path)
+	    return
+
         async def handle_get_peers(self, infohash, addr):
             await magnet_to_torrent('magnet:?xt=urn:btih:' + infohash.rstrip(), "./torrents/")
 
